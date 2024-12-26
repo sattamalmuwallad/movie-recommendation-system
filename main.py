339810e6fd96e01087315b4266c5e2982c7262fc
Sattam Almuwallad <sattam@sattams-MacBook-Pro.local>
2024-12-26 04:21:05 +0000

    Set up virtual environment and add requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+data = {
+    "Title": ["Inception", "The Dark Knight", "Interstellar", "Memento", "Dunkirk"],
+    "Genre": ["Sci-Fi", "Action", "Sci-Fi", "Thriller", "War"],
+    "Rating": [8.8, 9.0, 8.6, 8.4, 7.9]
+}
+movies = pd.DataFrame(data)
+movies.index = range(1,len(movies)+1) 
+#print(movies)
+import sys
+print(sys.executable)
